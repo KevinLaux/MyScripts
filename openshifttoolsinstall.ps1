1fe40f4927392add84ec6a6653778f4e97eecd10
kevinlaux <kevin.laux@metlife.com>
2019-09-04 14:12:00 -0400

    beta

--- openshifttoolsinstall.ps1
+++ openshifttoolsinstall.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding(DefaultParameterSetName='Install')]
 param(
-    [string] $Destination = "$($env:USERPROFILE)\Documents\OpenShiftClientTools\",
+    [string] $Path = "$($env:USERPROFILE)\Documents\OpenShiftClientTools\",
     [switch] $Admin,
     [Parameter(ParameterSetName='Install')]
     [switch] $Overwrite,
@@ -8,7 +8,7 @@ param(
                 ParameterSetName='Uninstall')]
     [switch] $Uninstall
 )
-if(-not ($Destination.EndsWith("\"))){$Destination = $Destination + "\"}
+if(-not ($Path.EndsWith("\"))){$Path = $Path + "\"}
 # Expand an archive using Expand-archive when available
 # and the DotNet API when it is not
 function Expand-ArchiveInternal {
@@ -33,7 +33,7 @@ function Expand-ArchiveInternal {
 function Test-OCAdminRights{
     [CmdletBinding()]
     param(
-        $Destination
+        $Path
     )
     Write-Verbose "Admin parameter was provided, testing Admin rights and setting installation location to Program Files"
     $user = [Security.Principal.WindowsIdentity]::GetCurrent()
@@ -42,33 +42,33 @@ function Test-OCAdminRights{
         Write-Warning "You are not running as Administrator either relaunch as administrator or remove the -Admin Parameter to install in user path"
         Exit
     }
-    if($Destination -eq "$($env:USERPROFILE)\Documents\OpenShiftClientTools\"){
-        $Destination = "$env:ProgramFiles\OpenShiftClientTools\"
+    if($Path -eq "$($env:USERPROFILE)\Documents\OpenShiftClientTools\"){
+        $Path = "$env:ProgramFiles\OpenShiftClientTools\"
     }
-    Return $Destination
+    Return $Path
 }
 function Test-OCInstalled{
     [CmdletBinding()]
     param(
-        $Destination,
+        $Path,
         $Overwrite
     )
     $oc = (Get-Command "oc.exe" -ErrorAction SilentlyContinue).Source -replace "oc.exe" , ""
     if(!($oc)){
-        if(Test-Path $Destination){$oc = $Destination}
+        if(Test-Path $Path){$oc = $Path}
     }
     if ($oc) 
     { 
         Write-Verbose "Found OC.exe"
-        if($($oc -eq $Destination) -and $Overwrite){
-            Remove-Item $Destination -Recurse -Force
+        if($($oc -eq $Path) -and $Overwrite){
+            Remove-Item $Path -Recurse -Force
         }
         else{
-            if($oc -eq $Destination){
-                Write-Warning "Installing will overwrite Openshift install at $Destination, use -Overwrite parameter to force installation"
+            if($oc -eq $Path){
+                Write-Warning "Installing will overwrite Openshift install at $Path, use -Overwrite parameter to force installation"
             }
             else{
-                Write-Warning "Openshift is already installed in path $oc, you are attempting to install in $destination. You may need to remove or change the path parameter"
+                Write-Warning "Openshift is already installed in path $oc, you are attempting to install in $Path. You may need to remove or change the destination parameter"
             }
             Exit
         }
@@ -104,34 +104,38 @@ function Get-OCInstall{
 function Set-OCEnvPath{
     [CmdletBinding()]
     param(
-        $Destination
+        $Path
     )
-    
-    #Set the Environment Variable path Permanently, only installed per user
-    $oldpath = (Get-ItemProperty -Path 'HKCU:\Environment' -Name PATH).path
-    $newpath = "$oldpath;$Destination"
-    $newpath = $newpath -replace ";;",";"
-    Set-ItemProperty -Path 'HKCU:\Environment' -Name PATH -Value $newPath
-
-    #Set the Environment Variable path temporarily needed until restart.
-    $oldpath = $env:Path
-    $newpath = "$oldpath;$Destination"
-    $newpath = $newpath -replace ";;",";"
-    $env:Path = $newpath
+    if(!(Get-Command "oc.exe" -ErrorAction SilentlyContinue)){
+        #Set the Environment Variable path Permanently, only installed per user
+        $oldpath = (Get-ItemProperty -Path 'HKCU:\Environment' -Name PATH).path
+        if($oldpath -notmatch $Path){
+            $newpath = "$oldpath;$Path"
+            $newpath = $newpath -replace ";;",";"
+            Set-ItemProperty -Path 'HKCU:\Environment' -Name PATH -Value $newPath
+        }
+        #Set the Environment Variable path temporarily needed until restart.
+        $oldpath = $env:Path
+        if($oldpath -notmatch $Path){
+            $newpath = "$oldpath;$Path"
+            $newpath = $newpath -replace ";;",";"
+            $env:Path = $newpath
+        }
+    }
 }
 function Remove-OCInstall{
     [CmdletBinding()]
     param(
-        $Destination
+        $Path
     )
-    if(Test-Path $Destination){
-        if(Get-ChildItem $Destination -Recurse | Where-Object name -eq 'oc.exe'){
-            Remove-Item $Destination -Force -Recurse
+    if(Test-Path $Path){
+        if(Get-ChildItem $Path -Recurse | Where-Object name -eq 'oc.exe'){
+            Remove-Item $Path -Force -Recurse
         }
     }
 }
 #TODO NEED TO PASS DESTINATION TO TEST-OCADMINRIGHTS
-$Destination = if($Admin){Test-OCAdminRights -Destination $Destination}
+if($Admin){$Path = Test-OCAdminRights -Path $Path}
 
 #Need to check where oc is installed if its in a known path we can remove it and the path from the environment variables.
 #If it is in an unknown path we should probably only remove the files and leave the env path
@@ -139,20 +143,20 @@ $Destination = if($Admin){Test-OCAdminRights -Destination $Destination}
 if($Uninstall){
      $oc = (Get-Command "oc.exe" -ErrorAction SilentlyContinue).Source -replace "oc.exe" , ""
      if($oc -eq "$env:ProgramFiles\OpenShiftClientTools\"){
-        Remove-OCInstall -Path $(Test-OCAdminRights -Destination $Destination)
+        Remove-OCInstall -Path $(Test-OCAdminRights -Path $Path)
         }
-     elseif($oc -eq "$($env:USERPROFILE)\Documents\OpenShiftClientTools\"){Remove-OCInstall -Path $Destination}
+     elseif($oc -eq "$($env:USERPROFILE)\Documents\OpenShiftClientTools\"){Remove-OCInstall -Path $Path}
 }
 else{
-    Test-OCInstalled -Destination $Destination -Overwrite $Overwrite
+    Test-OCInstalled -Path $Path -Overwrite $Overwrite
 
     $download_path = Get-OCInstall
 
     #Extract Files, uses User path so that Admin rights are not needed
-    if (-not (Test-Path -Path $Destination))
+    if (-not (Test-Path -Path $Path))
     {
-        New-Item -Path $Destination -ItemType Directory | Out-Null
+        New-Item -Path $Path -ItemType Directory | Out-Null
     }
-    Expand-ArchiveInternal -Path $download_path -DestinationPath $Destination
-    Set-OCEnvPath -Destination $Destination
+    Expand-ArchiveInternal -Path $download_path -DestinationPath $Path
+    Set-OCEnvPath -Path $Path
 }
