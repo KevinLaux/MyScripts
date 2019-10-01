dabb57b81393c666e53b050d4bda218927decb04
kevinlaux <kevin.laux@metlife.com>
2019-10-01 11:31:35 -0400

    updates

--- braincandy.ps1
+++ braincandy.ps1
@@ -2,10 +2,10 @@ $i=1
 $a = "         "
 Do{
     Write-host  "$($a.Substring(0, $a.Length - $i%10)) $($i) x " -ForegroundColor Black -BackgroundColor White -NoNewline
-    Write-Host "8" -ForegroundColor 'Red' -BackgroundColor White -NoNewline
+    Write-Host "8" -ForegroundColor Red -BackgroundColor White -NoNewline
     Write-Host " + " -ForegroundColor Black -BackgroundColor White -NoNewline
-    Write-host "$($i%10)" -ForegroundColor 'Blue' -BackgroundColor White -NoNewline
+    Write-host "$($i%10)" -ForegroundColor Blue -BackgroundColor White -NoNewline
     Write-Host " = $($i * 8 + $i%10)$($a.Substring(0, $a.Length - $i%10))" -ForegroundColor Black -BackgroundColor White
-    $i = $i = $i * 10 + $i%10 + 1
+    $i = $i * 10 + $i%10 + 1
 }
 While($i -le 123456789)
